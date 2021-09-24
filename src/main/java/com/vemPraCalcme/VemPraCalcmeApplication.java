9b0893faf542bb5a9c0c66e95cd17b842574857a
IGOR IVSON DA SILVA CUNHA <igorivson@outlook.com>
2021-09-24 19:58:05 -0300

    projeto criado

---
+++ src/main/java/com/vemPraCalcme/VemPraCalcmeApplication.java
@@ -0,0 +1,13 @@
+package com.vemPraCalcme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VemPraCalcmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VemPraCalcmeApplication.class, args);
+	}
+
+}
